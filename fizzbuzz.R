e50bc1fd344157994856d41ec6cb32aed052f90c
Michael Witherspoon II <mawitherspoonii@gmail.com>
2019-04-13 02:38:35 -0400

    Update fizzbuzz.R
    Even shorter:
    !i %% 3 evaluates to the same thing as i %% 3 == 0, so that was shortened
    i[all(i %% c(3, 5))] is preferable to i[!any(i %% c(3, 5) == 0)] as it is shorter

--- fizzbuzz.R
+++ fizzbuzz.R
@@ -1,1 +1,1 @@
-for(i in 1:100) {print(paste0("fizz"[i %% 3 == 0], "buzz"[i %% 5 == 0], i[!any(i %% c(3, 5) == 0)]))}
+for(i in 1:100) {print(paste0("fizz"[!i %% 3], "buzz"[!i %% 5], i[all(i %% c(3, 5))]))}
